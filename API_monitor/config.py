6590a6b46ee090aec324a8651c3805597ee6593a
Ritesh Rao V <ritheshraov016@gmail.com>
2025-03-23 14:07:40 +0530

    added encryption mechanism and fixed project structure

--- API_monitor/config.py
+++ API_monitor/config.py
@@ -1,5 +1,6 @@
 class APIMonitorConfig:
     SECRET_KEY = None
+    ENCRYPTION_KEY = None
     TRACKING_SERVER_URL = None
     MONITORED_APPS = []
     TAGS = []
@@ -7,7 +8,8 @@ class APIMonitorConfig:
     GEOIP_DB_PATH = "GeoLite2-City.mmdb"
 
     @classmethod
-    def initialize(cls, secret_key,tracking_server_url,monitored_apps,rate_limit_threshold, tags=None):
+    def initialize(cls, secret_key,encryption_key,tracking_server_url,monitored_apps,rate_limit_threshold, tags=None):
+        cls.ENCRYPTION_KEY = encryption_key
         cls.SECRET_KEY = secret_key
         cls.TRACKING_SERVER_URL = tracking_server_url
         cls.MONITORED_APPS = monitored_apps
